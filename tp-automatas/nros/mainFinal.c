6e320bc2b13793006729b5fc3e643bb0f8c866de
CamilaChoque <camilaichoquem@gmail.com>
2025-10-07 09:35:25 -0300

    Modificación: corrección lógica archivos + separación lógica operar aritmeticamente

--- tp-automatas/nros/mainFinal.c
+++ tp-automatas/nros/mainFinal.c
@@ -10,7 +10,7 @@
 #define MAX_ELEMENTOS 10
 #define MAX_LINE 1024
 
-// codigo punto 1 
+// codigo punto 1
 
 int es_miembro(char elemento, char lista[], int tamano) {
     for (int i = 0; i < tamano; i++) {
@@ -24,43 +24,43 @@ int es_miembro(char elemento, char lista[], int tamano) {
 void agregar_a_palabra(char* palabra, char c) {
     int len = strlen(palabra);
     palabra[len] = c;
-} 
+}
 
 int columnaDec(char c) {
-    if(c == '-') 
-        return 0; 
-    if(c == '0') 
+    if(c == '-')
+        return 0;
+    if(c == '0')
         return 1;
     if(isdigit(c))
         return 2;
-    if(c == '#') 
+    if(c == '#')
         return 3;
-    return 4; 
+    return 4;
 }
 
 int columna(char c) {
-    char nums[12] = {'A','B','C','D','E','F','a','b','c','d','e','f'}; 
-    if(c == '0') 
-        return 0; 
+    char nums[12] = {'A','B','C','D','E','F','a','b','c','d','e','f'};
+    if(c == '0')
+        return 0;
     if(isdigit(c) || es_miembro(c, nums , 12))
         return 2;
-    if(c == '#') 
+    if(c == '#')
         return 3;
     if(c == 'x' || c == 'X') {
-        return 1; 
+        return 1;
     }
-    return 5; 
+    return 5;
 }
 
 int columnaOctal(char c) {
-    char nums[7] = {'1','2','3','4','5','6','7'}; 
-    if(c == '0') 
-        return 0; 
+    char nums[7] = {'1','2','3','4','5','6','7'};
+    if(c == '0')
+        return 0;
     if(es_miembro(c, nums , 7))
         return 1;
-    if(c == '#') 
+    if(c == '#')
         return 2;
-    return 3; 
+    return 3;
 }
 
 int esDecimal(const char* token) {
@@ -77,18 +77,18 @@ int esDecimal(const char* token) {
     int i = 0; // índice del carácter actual
     int c = token[i]; // carácter actual
     while(c!='\0') {
-        e = ttdec[e][columnaDec(c)]; 
-        if(c != '#') 
-            agregar_a_palabra(palabra, c);        
+        e = ttdec[e][columnaDec(c)];
+        if(c != '#')
+            agregar_a_palabra(palabra, c);
         if(e == 0) {
             printf("DECIMAL: %s\n", palabra);
             memset(palabra, 0, sizeof(palabra)); // Reiniciar palabra
             countDecimal++;
         }
-        i++; 
-        c = token[i]; 
+        i++;
+        c = token[i];
     }
-    printf("Decimales: %d\n", countDecimal);
+    printf("DECIMALES: %d\n", countDecimal);
     if(e == 0) {
         return 1; // es decimal
     } else if(e == 4) {
@@ -110,16 +110,16 @@ int esOctal(const char* token) {
     int i = 0; // índice del carácter actual
     int c = token[i]; // carácter actual
     while(c!='\0') {
-        e = ttoct[e][columnaOctal(c)]; 
-        if(c != '#') 
-            agregar_a_palabra(palabra, c);        
+        e = ttoct[e][columnaOctal(c)];
+        if(c != '#')
+            agregar_a_palabra(palabra, c);
         if(e == 0) {
             printf("OCTAL: %s\n", palabra);
             memset(palabra, 0, sizeof(palabra)); // Reiniciar palabra
             countOctal++;
         }
-        i++; 
-        c = token[i]; 
+        i++;
+        c = token[i];
     }
     printf("Octales: %d\n", countOctal);
     if(e == 0) {
@@ -145,16 +145,16 @@ int esHexadecimal(const char* token) {
     int i = 0; // índice del carácter actual
     char c = token[i]; // carácter actual
     while(c!='\0') {
-        e = tthex[e][columna(c)]; 
-        if(c != '#') 
-            agregar_a_palabra(palabra, c);        
+        e = tthex[e][columna(c)];
+        if(c != '#')
+            agregar_a_palabra(palabra, c);
         if(e == 0) {
             printf("HEXADECIMAL: %s\n", palabra);
             memset(palabra, 0, sizeof(palabra)); // Reiniciar palabra
             countHex++;
         }
-        i++; 
-        c = token[i]; 
+        i++;
+        c = token[i];
     }
     printf("Hexadecimales: %d\n", countHex);
     if(e == 0) {
@@ -167,8 +167,8 @@ int esHexadecimal(const char* token) {
 
 void analizarCadena(const char* token) {
 
-    int countError = 0;
-    
+    int countDecimal = 0,  countError = 0;
+
     while (token != NULL) {
         if (esDecimal(token) == 2 && esOctal(token) != 1 && esHexadecimal(token) != 1) {
                 printf("ERROR LEXICO DECIMAL: %s\n", token);
@@ -179,11 +179,12 @@ void analizarCadena(const char* token) {
         } else if(esHexadecimal(token) == 2 && esDecimal(token) != 1 && esOctal(token) != 1) {
                 printf("ERROR LEXICO HEXADECIMAL: %s\n", token);
                 countError++;
-        } 
+        }
         token = strtok(NULL, "#");
     }
 
     printf("\nResumen:\n");
+    printf("Decimales: %d\n", countDecimal);
     printf("Errores: %d\n", countError);
 }
 
@@ -449,33 +450,6 @@ char* popToken(NodoPila** pila_){
     return token;
 }
 
-void operarSegunOperador(char* operador,char* operando1, char* operando2, char resultadoAlfa[MAX_LEN]){
-    printf("operador: %s\n",operador);
-    printf("operando1: %s\n",operando1);
-    printf("operando2: %s\n",operando2);
-    
-    int operador1 = string_a_int(operando1);
-    int operador2 = string_a_int(operando2);
-    int resultado=0;
-
-    switch(operador[0]) {
-        case '+': 
-            resultado = operador1 + operador2; 
-            break;
-        case '-': 
-            resultado = operador1 - operador2; 
-            break;
-        case '*': 
-            resultado = operador1 * operador2; 
-            break;
-        case '/': 
-            resultado = (operador2 != 0) ? operador1 / operador2 : 0; 
-            break;
-    }
-
-    sprintf(resultadoAlfa, "%d", resultado); //convertir en char
-}
-
 void leer_expresion(char* expresion) {
     printf("Ingrese una expresión aritmética: ");
     scanf("%s", expresion);
@@ -487,108 +461,112 @@ void leer_cadena_numerica(char* cadena) {
 }
 
 void leer_nombre_archivo(char* nombreArchivo) {
-    printf("Ingrese el nombre del archivo: ");
+    printf("Ingrese nuevo nombre del archivo: ");
     scanf("%s", nombreArchivo);
 }
 
-void mostrar_tokens(char tokens[MAX_TOKENS][MAX_LEN], int cantidad) {
-    printf("\nTokens:\n");
-    for (int i = 0; i < cantidad; i++) {
-        printf("%s ", tokens[i]);
-    }
-    printf("\n");
+int crear_archivo(char* nombreArchivo,char* expresion){
+    FILE* archivo;
+    archivo=fopen(nombreArchivo,"w");
+    fprintf(archivo,"%s",expresion);
+    printf("(Archivo %s creado)\n",nombreArchivo);
+    fclose(archivo);
+    return 1;
 }
-
-void mostrar_postfija(char postfija[MAX_TOKENS][MAX_LEN], int cantidad) {
-    printf("\nNotación postfija:\n");
-    for (int i = 0; i < cantidad; i++) {
-        printf("%s ", postfija[i]);
-    }
-    printf("\n");
-}
-
 int leer_expresion_archivo(char* expresion, char* nombreArchivo) {
     FILE* archivo = fopen(nombreArchivo, "r");
-    
+
     if (!archivo) {
         printf("Error: No se pudo abrir el archivo '%s'\n", nombreArchivo);
-        return 0;  
+        return 0;
     }
-    
-    if (!fgets(expresion, MAX_BUFFER, archivo)) {
+
+    if (!fgets(expresion, MAX_BUFFER-1, archivo)) {
         printf("Error: No se pudo leer del archivo\n");
         fclose(archivo);
         return 0;
+    }else{
+        printf("(Leyendo expresion del archivo %s...)\n\n",nombreArchivo);
     }
-    
     fclose(archivo);
     return 1;
 }
 
-void evaluar_expresion(char* expresion){
+void operarSegunOperador(char* operador,char* operando1, char* operando2, char resultadoAlfa[MAX_LEN]){
+    printf("operador: %s\n",operador);
+    printf("operando1: %s\n",operando1);
+    printf("operando2: %s\n",operando2);
+    int resultado=0;
+    if(operador[0]=='+'){
+        resultado=string_a_int(operando1)+string_a_int(operando2);
+    }
+    if(operador[0]=='*'){
+        resultado=string_a_int(operando1)*string_a_int(operando2);
+    }
+    if(operador[0]=='/'){
+        resultado=string_a_int(operando1)/string_a_int(operando2);
+    }
+    if(operador[0]=='-'){
+        resultado=string_a_int(operando1)-string_a_int(operando2);
+    }
+    sprintf(resultadoAlfa, "%d", resultado); //convertir en char
+
+}
+
+char* operarPolacaInversa(int cantTokens,char tokens[MAX_TOKENS][MAX_LEN]){
+    char postfija[MAX_TOKENS][MAX_LEN];
     NodoPila*pila=NULL;
     NodoPila*pilaInvertida=NULL;
     NodoPila*pilaAux=NULL;
-
     char* token;
-    char resultado[MAX_LEN];
-    char tokens[MAX_TOKENS][MAX_LEN],postfija[MAX_TOKENS][MAX_LEN];
+    static char resultado[MAX_LEN];
 
+    infijaToPostfija(tokens,cantTokens,postfija); //Lpostfija es lo mismo que cantTokens
 
-    int cantTokens = tokenizar(expresion, tokens);
-    mostrar_tokens(tokens, cantTokens);
-
-	int Lpostfija=infijaToPostfija(tokens,cantTokens,postfija); //Lpostfija es lo mismo que cantTokens
-	printf("Notacion polaca inversa (push pila): ");
-	for (int k = 0; k < Lpostfija; k++) {
-        printf("%s ", postfija[k]);
+	//printf("Notacion polaca inversa (push pila): ");
+	//ponemos del ultimo elemento al inicial en la pila
+	for (int k = cantTokens; k >=0; k--) {
         pushToken(&pila,postfija[k]);
     }
-
-    printf("\n\nPilaInvertida: ");
-	for (int k = 0; k < Lpostfija; k++) {
+    //printf("\nEvaluando cada componente:\n");
+    //evaluamos cada componente y segun el operadora, operamos aritmeticamente
+    for (int k = 0; k < cantTokens; k++) {
         token=popToken(&pila);
-        printf("%s ", token);
-        pushToken(&pilaInvertida,token);
-    }
-
-    printf("\nEvaluando cada componente:\n");
-    for (int k = 0; k < Lpostfija; k++) {
-
-        token=popToken(&pilaInvertida);
-        printf("Elemento de la pilaInvertida: %s\n",token);
         if(!esOperador(token)){
-            printf("A poner pilaAux: ");
             pushToken(&pilaAux,token);
-            printf("%s", token);
         }else{
-            char* operador2 = popToken(&pilaAux);
-            char* operador1 = popToken(&pilaAux);
-            operarSegunOperador(token,operador1,operador2,resultado);
-            printf("resultado: %s\n",resultado);
+            operarSegunOperador(token,popToken(&pilaAux),popToken(&pilaAux),resultado);
             pushToken(&pilaAux,resultado);
-            free(operador1);
-            free(operador2);
         }
-        printf("\n-fin de analisis-\n");
-        free(token);
     }
+
+    return resultado;
+}
+void operar_expresion(char* expresion){
+    //falta funcion evaluar expresion: que sea solo deciamles
+    char tokens[MAX_TOKENS][MAX_LEN];
+    int cantTokens=0;
+    cantTokens = tokenizar(expresion, tokens);
+
+    printf("Resultado: %s",operarPolacaInversa(cantTokens,tokens));
+
 }
 
 int main() {
 
     char expresion[MAX_BUFFER];
+    char nombreArchivo[MAX_BUFFER];
     int opcion;
-    
+
     printf("TP AUTOMATAS - SINTAXIS Y SEMANTICA DE LOS LENGUAJES\n\n");
     printf("Seleccione una opcion:\n");
     printf("1. Analizar cadena con numeros\n");
     printf("2. Evaluar expresion aritmetica dada por terminal\n");
-    printf("3. Evaluar expresion aritmetica dada por archivo\n");
+    printf("3. Evaluar expresion aritmetica ingresada por archivo\n");
     printf("4. Salir\n");
     printf("\nOpcion: ");
     scanf("%d", &opcion);
-    
+
     switch(opcion) {
         case 1: {
             char cadena[MAX_LINE];
@@ -598,15 +576,16 @@ int main() {
         }
         case 2:
             leer_expresion(expresion);
-            evaluar_expresion(expresion);
+            operar_expresion(expresion);
             break;
         case 3:
-            /* char nombreArchivo[MAX_BUFFER];
             leer_nombre_archivo(nombreArchivo);
+            leer_expresion(expresion);
+            crear_archivo(nombreArchivo,expresion);
+
             leer_expresion_archivo(expresion, nombreArchivo);
-            evaluar_expresion(expresion);
+            operar_expresion(expresion);
             break;
-            */ 
         case 4:
             printf("\nSaliendo...\n");
             break;
